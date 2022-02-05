16ae81868189f9afff7c214a1776fd0079dfac0d
MIRIAM NAKI <nakiyingim@gmail.com>
2022-02-05 14:36:23 -0800

    add donation form

--- client/src/pages/DonationPage/DonationPage.js
+++ client/src/pages/DonationPage/DonationPage.js
@@ -9,10 +9,12 @@ class DonationPage extends Component {
   }
 
   fetchOrganization = (id) => {
-    axios.get( `http://localhost:8080/donation/${id}`)
+    axios.get( `http://localhost:8080/account/${id}`)
     .then(res => {
       console.log(res.data)
       this.setState({organization: res.data})
+    }).catch(err => {
+      console.log(err)
     })
 
   }
@@ -23,8 +25,29 @@ class DonationPage extends Component {
 
   render(){
     console.log(this.props)
+    if(!this.state.organization){
+      console.log(this.state.organization)
+      return null  
+    }
+    const {description, location, program_name} = this.state.organization
+
+    console.log('descr',description)
+    
     return (
       <>
+      {this.state.organization && 
+       <section className="donate"> 
+        <div className="donate__wrapper">
+         <div className="donate__image"></div>
+          <div className="donate__wrapper2">
+            <h3 className="donate__subtitle">Name: {program_name}</h3>
+            <h3 className="donate__subtitle">Location: {location}</h3>
+            <p className='donate__text' >{description}</p>
+          </div>
+        </div>
+      </section>
+
+     }
         <DonationForm/>
       </>
     );
