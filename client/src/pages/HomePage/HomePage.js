b60f9336ed566f253f9fc621d382e7399a9010e2
MIRIAM NAKI <nakiyingim@gmail.com>
2022-02-05 03:40:40 -0800

    add explore section

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './HomePage.scss';
+import { Link } from "react-router-dom";
 
 
 const HomePage = () => {
@@ -7,7 +8,9 @@ const HomePage = () => {
     <main className="main">
       <section className="hero">
         <div className="hero__image"></div>
-        <p className="hero__text">Learn more</p>   
+        <Link to='/organizations'>
+          <p className="hero__text">Learn more</p>   
+        </Link>
       </section>
 
       <section className="about"> 
@@ -18,10 +21,15 @@ const HomePage = () => {
             <h4 className="about__subtitle">Who we are</h4>
             <p className='about__text' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita dolorem aspernatur nemo, error est doloremque ab eaque placeat voluptas! Tempore itaque porro assumenda fugiat vitae sapiente ad, laudantium officiis harum perferendis! Maiores maxime inventore molestias. Architecto, vitae eos. At molestiae esse commodi culpa provident quis officiis laudantium magnam quia dolores ipsum accusamus nobis corporis minus ipsam facilis repellat deserunt error quos quaerat eligendi distinctio, vero possimus repudiandae. Ab, ut quasi aut repudiandae eligendi modi. Cum reiciendis nisi repudiandae voluptatibus quasi animi possimus. Illum sit necessitatibus maxime doloremque reprehenderit suscipit perspiciatis placeat quibusdam accusantium, magnam eum minima iure nemo? Repellat quaerat nisi eos consectetur possimus qui, sunt tempora! Distinctio repudiandae debitis nesciunt sint. Provident quod amet asperiores tempora assumenda saepe possimus!</p>
           </div>
+        </div>
+      </section>
 
-          </div>
-
-
+      <section className="explore">
+       <div className="explore__wrapper">
+         <h4 className="explore__title">Explore Organizations</h4>
+         <p className="explore__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis voluptatum repudiandae nulla molestiae numquam sapiente, tempore quidem nostrum, illum fugit, harum impedit cupiditate quod excepturi delectus voluptates mollitia iste atque praesentium accusantium minus aperiam iure. Labore ipsum quam repudiandae dolor, repellat qui incidunt veritatis, eius nihil illo voluptas dolores animi.</p>
+       </div>
+       <div className="explore__image"></div>
       </section>
 
     </main>
