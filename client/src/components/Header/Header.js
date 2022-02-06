610002ce86a99c32167dfbb39e1f0fa1ab5e2e67
Phoebe Chang <phoebe03111@gmail.com>
2022-02-05 17:42:09 -0800

    Change font, add images and styling for home page

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Header.scss";
 import { Link } from "react-router-dom";
-import logo from "../../assets/logo.png";
+import logo from "../../assets/images/logo.png";
 
 const Header = () => {
   return (
