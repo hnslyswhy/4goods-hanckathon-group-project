1426906d0c19b6e1b74df61cab3b0ee96f0c265f
CDeweger <caroljydeweger@gmail.com>
2022-02-06 02:57:18 -0800

    update profile page styling

--- client/src/pages/DonationPage/DonationPage.js
+++ client/src/pages/DonationPage/DonationPage.js
@@ -7,7 +7,7 @@ class DonationPage extends Component {
   state = {
     organization: null,
     donations: [],
-    accountObj: null
+    accountObj: null,
   };
 
   fetchData = () => {
@@ -20,17 +20,21 @@ class DonationPage extends Component {
         // console.log(res.data);
         // console.log(res.data.account);
         this.setState({ organization: res.data.account });
-        axios.get(`http://localhost:8080/auth/login/${this.state.organization}`)
-        .then((res) => {
-          console.log(res);
-          this.setState({accountObj: res.data})
-          axios.get(`http://localhost:8080/donation/account/${res.data.accountId}`)
+        axios
+          .get(`http://localhost:8080/auth/login/${this.state.organization}`)
           .then((res) => {
-            console.log(res.data);
-            this.setState({ donations: res.data });
-          })
-        })
-      })  
+            console.log(res);
+            this.setState({ accountObj: res.data });
+            axios
+              .get(
+                `http://localhost:8080/donation/account/${res.data.accountId}`
+              )
+              .then((res) => {
+                console.log(res.data);
+                this.setState({ donations: res.data });
+              });
+          });
+      })
       .catch((err) => {
         console.log(err);
       });
@@ -40,48 +44,60 @@ class DonationPage extends Component {
     this.fetchData();
   }
   render() {
-    console.log(this.props)
+    console.log(this.props);
     if (!this.state.organization) {
       return null;
     }
     if (!this.state.donations) {
       return null;
     }
-    
 
     return (
-      
       <>
-      
-        {this.state.accountObj && ( <>
-          <section className="donation">
-            <div className="donation__wrapper">
-              <img className="donation__image" src={this.state.accountObj.image} alt={this.state.accountObj.program_name} />
-              <div className="donation__wrapper2">
-                <h4 className="donation__subtitle">Name: {this.state.accountObj.program_name}</h4>
-                <h4 className="donation__subtitle">Location: {this.state.accountObj.location}</h4>
-                <p className="donation__text">{this.state.accountObj.description}</p>
+        {this.state.accountObj && (
+          <>
+            <section className="donation">
+              <div className="donation__wrapper">
+                <img
+                  className="donation__image"
+                  src={this.state.accountObj.image}
+                  alt={this.state.accountObj.program_name}
+                />
+                <div className="donation__wrapper2">
+                  <h4 className="donation__subtitle">
+                    Name: {this.state.accountObj.program_name}
+                  </h4>
+                  <h4 className="donation__subtitle">
+                    Location: {this.state.accountObj.location}
+                  </h4>
+                  <p className="donation__text">
+                    {this.state.accountObj.description}
+                  </p>
+                </div>
               </div>
-            </div>
-          </section>
-           <div className="donation__component">
-           <DonationForm accountId={this.props} account={this.state.accountObj.accountId}/>
-         </div>
-         </>
-        )}
-        
-        {this.state.donations.map((donation) => {
-          return (
-            <span className="donation__card">
-              <CardItem
-                image={donation.image}
-                name={donation.itemName}
-                status={donation.status}
-                description={donation.information}
+            </section>
+            <div className="donation__component">
+              <DonationForm
+                accountId={this.props}
+                account={this.state.accountObj.accountId}
               />
-            </span>
-          );
-        })}
+            </div>
+          </>
+        )}
+        <div className="donation__posts">
+          {this.state.donations.map((donation) => {
+            return (
+              <span className="donation__card">
+                <CardItem
+                  image={donation.image}
+                  name={donation.itemName}
+                  status={donation.status}
+                  description={donation.information}
+                />
+              </span>
+            );
+          })}
+        </div>
       </>
     );
   }
